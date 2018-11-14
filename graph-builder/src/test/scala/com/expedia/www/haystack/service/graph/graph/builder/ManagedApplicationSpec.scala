c80ec4fbde9b9517160d47f2c1b0849a5083da33
jeffbaker <jeffbaker@expedia.com>
2018-11-14 10:27:22 -0800

    Finished unit tests for ManagedApplication; also sorted variables in pom.xml for better readability.

--- graph-builder/src/test/scala/com/expedia/www/haystack/service/graph/graph/builder/ManagedApplicationSpec.scala
+++ graph-builder/src/test/scala/com/expedia/www/haystack/service/graph/graph/builder/ManagedApplicationSpec.scala
@@ -1,35 +1,36 @@
 package com.expedia.www.haystack.service.graph.graph.builder
 
+import java.security.Permission
+
 import com.codahale.metrics.JmxReporter
 import com.expedia.www.haystack.commons.kstreams.app.ManagedService
 import com.expedia.www.haystack.service.graph.graph.builder.ManagedApplication._
-import org.mockito.Mockito.{verify, verifyNoMoreInteractions}
+import org.mockito.Mockito.{times, verify, verifyNoMoreInteractions, when}
 import org.scalatest.mockito.MockitoSugar
-import org.scalatest.{BeforeAndAfter, FunSpec}
+import org.scalatest.{BeforeAndAfterAll, FunSpec, SequentialNestedSuiteExecution}
 import org.slf4j.Logger
 
-class ManagedApplicationSpec extends FunSpec with MockitoSugar with BeforeAndAfter {
+sealed case class ExitException(status: Int) extends SecurityException("System.exit() was called") {
+}
 
-  var service: ManagedService = _
-  var stream: ManagedService = _
-  var jmxReporter: JmxReporter = _
-  var logger: Logger = _
+sealed class NoExitSecurityManager extends SecurityManager {
+  override def checkPermission(perm: Permission): Unit = {}
 
-  before {
-    service = mock[ManagedService]
-    stream = mock[ManagedService]
-    jmxReporter = mock[JmxReporter]
-    logger = mock[Logger]
+  override def checkPermission(perm: Permission, context: Object): Unit = {}
+
+  override def checkExit(status: Int): Unit = {
+    super.checkExit(status)
+    throw ExitException(status)
   }
+}
 
-  after {
-    verifyNoMoreInteractions(service)
-    verifyNoMoreInteractions(stream)
-    verifyNoMoreInteractions(jmxReporter)
-    verifyNoMoreInteractions(logger)
-  }
+class ManagedApplicationSpec extends FunSpec with MockitoSugar with BeforeAndAfterAll with SequentialNestedSuiteExecution {
+
+  override def beforeAll(): Unit = System.setSecurityManager(new NoExitSecurityManager())
+  override def afterAll(): Unit = System.setSecurityManager(null)
 
   describe("ManagedApplication constructor") {
+    val (mocks: List[AnyRef], service: ManagedService, stream: ManagedService, jmxReporter: JmxReporter, logger: Logger) = createAndBundleMocks
     it ("should throw an IllegalArgumentException if passed a null service") {
       assertThrows[IllegalArgumentException] {
         new ManagedApplication(null, stream, jmxReporter, logger)
@@ -50,11 +51,13 @@ class ManagedApplicationSpec extends FunSpec with MockitoSugar with BeforeAndAft
         new ManagedApplication(service, stream, jmxReporter, null)
       }
     }
+    verifyNoMoreInteractionsForAllMocks(mocks)
   }
 
   describe("ManagedApplication start") {
+    val (mocks: List[AnyRef], service: ManagedService, stream: ManagedService, jmxReporter: JmxReporter, logger: Logger) = createAndBundleMocks
+    val managedApplication = new ManagedApplication(service, stream, jmxReporter, logger)
     it ("should start all dependencies when called") {
-      val managedApplication = new ManagedApplication(service, stream, jmxReporter, logger)
       managedApplication.start()
       verify(service).start()
       verify(logger).info(StartMessage)
@@ -63,9 +66,18 @@ class ManagedApplicationSpec extends FunSpec with MockitoSugar with BeforeAndAft
       verify(jmxReporter).start()
       verify(logger).info(StreamStartMessage)
     }
+    it ("should call System.exit() when an exception is thrown") {
+      when(service.start()).thenThrow(new NullPointerException)
+      assertThrows[ExitException] {
+        managedApplication.start()
+      }
+      verify(service, times(2)).start()
+    }
+    verifyNoMoreInteractionsForAllMocks(mocks)
   }
 
   describe("ManagedApplication stop") {
+    val (mocks: List[AnyRef], service: ManagedService, stream: ManagedService, jmxReporter: JmxReporter, logger: Logger) = createAndBundleMocks
     it ("should stop all dependencies when called") {
       val managedApplication = new ManagedApplication(service, stream, jmxReporter, logger)
       managedApplication.stop()
@@ -77,5 +89,37 @@ class ManagedApplicationSpec extends FunSpec with MockitoSugar with BeforeAndAft
       verify(jmxReporter).close()
       verify(logger).info(LoggerStopMessage)
     }
+    verifyNoMoreInteractionsForAllMocks(mocks)
   }
+
+  private def createMocks(): List[AnyRef] =
+  {
+    val service = mock[ManagedService]
+    val stream = mock[ManagedService]
+    val jmxReporter = mock[JmxReporter]
+    val logger = mock[Logger]
+    List(service, stream, jmxReporter, logger)
+  }
+
+  private def createAndBundleMocks = {
+    val mocks = createMocks()
+    val (service: ManagedService, stream: ManagedService, jmxReporter: JmxReporter, logger: Logger) = bundleMocks(mocks)
+    (mocks, service, stream, jmxReporter, logger)
+  }
+
+  private def bundleMocks(mocks: List[AnyRef]) = {
+    val service = mocks.head.asInstanceOf[ManagedService]
+    val stream = mocks(1).asInstanceOf[ManagedService]
+    val jmxReporter = mocks(2).asInstanceOf[JmxReporter]
+    val logger = mocks(3).asInstanceOf[Logger]
+    (service, stream, jmxReporter, logger)
+  }
+
+  private def verifyNoMoreInteractionsForAllMocks(mocks: List[AnyRef]): Unit = {
+    verifyNoMoreInteractions(mocks.head)
+    verifyNoMoreInteractions(mocks(1))
+    verifyNoMoreInteractions(mocks(2))
+    verifyNoMoreInteractions(mocks(3))
+  }
+
 }
